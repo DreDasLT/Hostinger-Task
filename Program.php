fde9c0fcd3a7038a84ec8337f62716efdf1ea366
Vilius <dre2kas@gmail.com>
2018-10-30 23:33:47 +0200

    created category add form with sessions

--- Program.php
+++ Program.php
@@ -9,21 +9,58 @@
 require_once('Category.php');
 require_once('functions.php');
 
-$categories = array(
-    new Category(1, 'cat'),
-        new Category(2, 'sub-cat', 1),
-            new Category(3, 'sub-sub-cat', 2),
-        new Category(4, 'sub-cat', 1),
-            new Category(5, 'sub-sub-cat', 4),
-                new Category(9, 'sub-sub-sub-cat', 5),
-                    new Category(10, 'sub-sub-sub-sub-cat', 9),
-                        new Category(11, 'sub-sub-sub-sub-sub-cat', 10),
-            new Category(6, 'sub-sub-cat', 4),
-            new Category(7, 'sub-sub-cat', 4),
-    new Category(8, 'cat')
-);
+session_start();
 
-$tree = printTree($categories);
+
+if (!isset($_SESSION['categories'])) {
+    $_SESSION['categories'] = $categories;
+} else {
+    $categories = $_SESSION['categories'];
+}
+
+$categoryFromList = "";
+$category = "";
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST["category"])) {
+        $category = test_input($_POST["category"]);
+        if (!preg_match("/^[a-zA-Z0-9 -]*$/",$category)) {
+            $error = "Tik raidės, skaičiai, tarpas ir brukšnelis";
+        }
+    } else {
+        $error = "Tuščias laukelis";
+    }
+
+    if (!empty($_POST["categoryFromList"])) {
+        $categoryFromList = test_input($_POST["categoryFromList"]);
+    }
+    if($categoryFromList == 0) {
+        array_push($categories, new Category(count($categories)+1, $category));
+    } else {
+        array_push($categories, new Category(count($categories)+1, $category, $categoryFromList));
+    }
+    $_SESSION['categories'] = $categories;
+}
+
+
+
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Parent category:
+    <select name="categoryFromList">
+        <option value="0">Root</option>
+        <?php printTreeToSelectForm($categories); ?>
+    </select>
+    <br><br>
+    Category Name: <input type="text" name="category" value="<?php echo $category;?>">
+    <span> <?php echo $error;?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Pridėti">
+</form>
+
+<?php
 
 
 
--- functions.php
+++ functions.php
@@ -8,6 +8,20 @@
 
 require_once('Category.php');
 
+$categories = array(
+    new Category(1, 'cat'),
+    new Category(2, 'sub-cat', 1),
+    new Category(3, 'sub-sub-cat', 2),
+    new Category(4, 'sub-cat', 1),
+    new Category(5, 'sub-sub-cat', 4),
+    new Category(6, 'sub-sub-cat', 4),
+    new Category(7, 'sub-sub-cat', 4),
+    new Category(8, 'cat'),
+    new Category(9, 'sub-sub-sub-cat', 5),
+    new Category(10, 'sub-sub-sub-sub-cat', 9),
+    new Category(11, 'sub-sub-sub-sub-sub-cat', 10),
+);
+
 /**
  * @param array $categories
  * @param int $parent
@@ -24,10 +38,44 @@ function printTree(array $categories, int $parent = 0) {
 }
 
 /**
+ * @param array $categories
+ * @param int $parent
+ * @return bool
+ */
+function printTreeToSelectForm(array $categories, int $parent = 0) {
+    foreach ($categories as $category) {
+        if ($category->getParentCategoryId() == $parent) {
+            printBranchToSelectForm($category);
+            printTreeToSelectForm($categories, $category->getId());
+        }
+    }
+    return true;
+}
+
+
+/**
  * @param Category $category
  */
 function printBranch(Category $category) {
     $parent = $category->getParentCategoryId();
     $tabNumber = $parent != 0 ? $parent + 1 : 0;
     echo str_repeat('&nbsp', $tabNumber) . $category->getName() . "<br>";
+}
+
+/**
+ * @param Category $category
+ */
+function printBranchToSelectForm(Category $category) {
+    $parent = $category->getParentCategoryId();
+    $tabNumber = $parent != 0 ? $parent + 1 : 0;
+    echo '<option value="'. $category->getId() . '">'  . str_repeat('&nbsp', $tabNumber) . $category->getName() . "</option>";
+}
+
+
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
